29914a6cbbbc5dcebb611943b0008967caf34354
F4 Developer <root@f4dev.me>
2018-08-06 22:11:10 +0200

    :heavy_plus_sign: add main class
    initConfig() method added

---
+++ src/main/java/me/f4dev/plugincontroller/PluginController.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of PluginController project by F4 Developer which is released under GNU General Public License v3.0.
+ * See file LICENSE for full license details.
+ */
+
+/*
+ * This file is part of PluginController project by F4 Developer which is released under GNU General Public License v3.0.
+ * See file LICENSE for full license details.
+ */
+
+package me.f4dev.plugincontroller;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class PluginController extends JavaPlugin {
+  
+  public FileConfiguration language;
+  
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+  }
+  
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+  
+  private void initConfig() {
+    if(!new File(getDataFolder(), "config.yml").exists()) {
+      saveResource("config.yml", false);
+    }
+    
+    String[] langArray = {"en_US"};
+    for(String lang : langArray) {
+      if(!new File(getDataFolder() + File.separator + "languages", lang + ".yml").exists()) {
+        saveResource("languages" + File.separator + lang + ".yml", false);
+      }
+    }
+    
+    language = YamlConfiguration.loadConfiguration(new File(getDataFolder() + File.separator +
+            "languages", this.getConfig().getString("Language") + ".yml"));
+  }
+}
