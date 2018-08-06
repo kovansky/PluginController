50766da9c285f938a8a9313f2440919b90c3c334
F4 Developer <root@f4dev.me>
2018-08-06 23:28:34 +0200

    :heavy_plus_sign: Create ListManager class

--- src/main/java/me/f4dev/plugincontroller/utils/Controller.java
+++ src/main/java/me/f4dev/plugincontroller/utils/Controller.java
@@ -5,5 +5,32 @@
 
 package me.f4dev.plugincontroller.utils;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
 public class Controller {
+//  public void enablePlugin( final Plugin plugin )
+//  {
+//    Bukkit.getPluginManager( ).enablePlugin( plugin );
+//    File file = new File( this.plugin.getDataFolder( ), "List.yml");
+//    if( file.exists( ) )
+//    {
+//      FileConfiguration disabled = YamlConfiguration.loadConfiguration( file );
+//      List<String> list = disabled.getStringList( "Disabled" );
+//      if( list.contains( plugin.getName( ) ) )
+//        list.remove( plugin.getName( ) );
+//      disabled.set( "Disabled", list );
+//
+//      try {
+//        disabled.save( file );
+//      } catch( Exception e ) {
+//        e.printStackTrace( );
+//      }
+//    }
+//  }
+  
+  public static void enablePlugin(final Plugin plugin) {
+    Bukkit.getPluginManager().enablePlugin(plugin);
+    
+  }
 }
