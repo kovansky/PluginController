2dd077e9d9643d2a93f053a15e1095b4cd6b126f
F4 Developer <root@f4dev.me>
2018-08-06 22:44:19 +0200

    :fire: Removing unnecesary

--- src/main/java/me/f4dev/plugincontroller/PluginController.java
+++ src/main/java/me/f4dev/plugincontroller/PluginController.java
@@ -3,26 +3,6 @@
  * See file LICENSE for full license details.
  */
 
-/*
- * This file is part of PluginController project by F4 Developer which is released under GNU General Public License v3.0.
- * See file LICENSE for full license details.
- */
-
-/*
- * This file is part of PluginController project by F4 Developer which is released under GNU General Public License v3.0.
- * See file LICENSE for full license details.
- */
-
-/*
- * This file is part of PluginController project by F4 Developer which is released under GNU General Public License v3.0.
- * See file LICENSE for full license details.
- */
-
-/*
- * This file is part of PluginController project by F4 Developer which is released under GNU General Public License v3.0.
- * See file LICENSE for full license details.
- */
-
 package me.f4dev.plugincontroller;
 
 import org.bukkit.ChatColor;
