c512afeecc8656d68f6a41fdea4b5d7e3b8c4ace
F4 Developer <root@f4dev.me>
2018-08-10 22:39:13 +0200

    :sparkles: CommandPreprocessListener listens to two events - PlayerCommandPreprocessEvent and ConsoleCommandEvent

--- src/main/java/me/f4dev/plugincontroller/listeners/CommandPreprocessListener.java
+++ src/main/java/me/f4dev/plugincontroller/listeners/CommandPreprocessListener.java
@@ -10,12 +10,13 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 
 import java.io.File;
 import java.io.IOException;
 
 public class CommandPreprocessListener implements Listener {
-  PluginController plugin;
+  private PluginController plugin;
   
   public CommandPreprocessListener(PluginController plugin) {
     this.plugin = plugin;
@@ -23,17 +24,37 @@ public class CommandPreprocessListener implements Listener {
   }
   
   @EventHandler
+  public void onServerCommand(ServerCommandEvent e) {
+    String command = e.getCommand();
+    String[] args = command.split(" ");
+    
+    if(args.length == 1) {
+      if(args[0].equalsIgnoreCase("reload") || args[0].equalsIgnoreCase("rl")) {
+        File listFile = new File(plugin.getDataFolder(), "list.yml");
+        if(listFile.exists()) {
+          FileConfiguration disabledPlugins = YamlConfiguration.loadConfiguration(listFile);
+          disabledPlugins.set("reload", true);
+          try {
+            disabledPlugins.save(listFile);
+          } catch(IOException ex) {
+            ex.printStackTrace();
+          }
+        }
+      }
+    }
+  }
+  
+  @EventHandler
   public void onCommandPreprocess(PlayerCommandPreprocessEvent e) {
     String message = e.getMessage();
     String[] args = message.split(" ");
-    
+  
     if(args.length == 1) {
       if(args[0].equalsIgnoreCase("/reload") || args[0].equalsIgnoreCase("/rl")) {
         File listFile = new File(plugin.getDataFolder(), "list.yml");
         if(listFile.exists()) {
           FileConfiguration disabledPlugins = YamlConfiguration.loadConfiguration(listFile);
           disabledPlugins.set("reload", true);
-  
           try {
             disabledPlugins.save(listFile);
           } catch(IOException ex) {
